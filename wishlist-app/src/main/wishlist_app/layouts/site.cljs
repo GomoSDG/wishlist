77926b09857fe9af8403d8753f2b6c5f81662b34
Gomotso <gomotsol@navigare.co.za>
2020-07-29 00:44:44 +0200

    update: add bulma, correct asset and css path.

--- wishlist-app/src/main/wishlist_app/layouts/site.cljs
+++ wishlist-app/src/main/wishlist_app/layouts/site.cljs
@@ -1,15 +1,29 @@
 (ns wishlist-app.layouts.site)
 
 (defn navbar []
-  [:nav.navbar.navbar-dark.bg-primary
-   [:a.navbar-brand {:href "#"}
-    "g5g"]
-   [:div.collapse.navbar-collapse
-    [:ul.navbar-nav
-     [:li.nav-item.active
-      [:a.nav-link {:href "#"}
-       "Home"]]]]])
+  [:nav.navbar.is-light {:aria-label "main navigation",
+                         :role       "navigation"}
+   [:div.container
+    [:div.navbar-brand
+     [:a.navbar-item {:href "#"}
+      [:img {:src "/img/logo.png"}]]
+    [:a.navbar-burger.burger
+     {:data-target   "uiNav",
+      :aria-expanded "false",
+      :aria-label    "menu",
+      :role          "button"}
+     [:span {:aria-hidden "true"}]
+     [:span {:aria-hidden "true"}]
+     [:span {:aria-hidden "true"}]]]
+   [:div#uiNav.navbar-menu
+    [:div.navbar-start [:a.navbar-item "Home"]]
+    [:div.navbar-end
+     [:div.navbar-item
+      [:div.buttons
+       [:a.button.is-primary [:strong "Sign up"]]
+       [:a.button.is-light "Log in"]]]]]]])
 
 (defn site-layout [panel]
   [:<>
-   [panel]])
+   [navbar]
+   [:div.container [panel]]])
--- wishlist-app/src/main/wishlist_app/panels/home.cljs
+++ wishlist-app/src/main/wishlist_app/panels/home.cljs
@@ -1,5 +1,7 @@
 (ns wishlist-app.panels.home)
 
 (defn home []
-  [:h1 "Hello, Gomosdg!"])
+  [:div.columns
+   [:div.column.has-background-primary ]
+   [:div.column.is-three-quarters.has-background-info]])
 
