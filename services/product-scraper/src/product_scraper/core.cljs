a17aedfbf34c2f3086dd626bfb92debb1ee029ab
Gomotso <gomotsol@navigare.co.za>
2020-08-28 22:48:40 +0200

    Update services

--- services/product-scraper/src/product_scraper/core.cljs
+++ services/product-scraper/src/product_scraper/core.cljs
@@ -1,21 +1,39 @@
 (ns product-scraper.core
   (:require ["express" :as express]
             ["apify" :as apify]
+            [cljs.core.async :refer [go <!]]
+            [cljs.core.async.interop :refer-macros [<p!]]
             [product-scraper.crawler.core :as crawler]))
 
 (set! *warn-on-infer* true)
 
 (defonce server (atom nil))
 
+(defonce request-queue (atom nil))
+
+
+(defn scrape-url [req res]
+  (go
+    (let [{url "url"} (js->clj (.-params req))
+          _           (.addRequest @request-queue #js {:url url})
+          crawler     (crawler/create-crawler {:request-queue url
+                                               :handler       crawler/handle-page-fn})]
+      (.run crawler)
+      (.send res url))))
+
 (defn start-server []
   (println "Starting server")
   (let [app (express)]
     (.get app "/" (fn [req res] (.send res "Hello, world")))
+    (.get app "/ts/:url(*)" scrape-url)
     (.listen app 3003 (fn [] (println "Example app listening on port 3000!")))))
 
 (defn start! []
   ;; called by main and after reloading code
-  (reset! server (start-server)))
+  (go
+    (let []
+      (reset! request-queue )
+      (reset! server (start-server))))
 
 (defn stop! []
   ;; called before reloading code
@@ -23,8 +41,6 @@
   (reset! server nil))
 
 (def crawler (crawler/default-crawler "https://cljs.github.io/api/cljs.core/new"))
-
-#_(def list (crawler/create-request-list "https://cljs.github.io/api/cljs.core/new"))
 
 (crawler/run-crawler crawler)
 
--- services/product-scraper/src/product_scraper/crawler/core.cljs
+++ services/product-scraper/src/product_scraper/crawler/core.cljs
@@ -6,35 +6,36 @@
 
 (def promisify util/promisify)
 
-(defn create-crawler [h r & [maxRequests maxConcurrency]]
-  (let [maxR (or maxRequests 100)
-        maxC (or maxConcurrency 1)]
-    (println (clj->js {:requestList r
-                       :handlePageFunction h
-                       :launchPuppeteerOptions {:headless true
-                                                :stealth true}}))
-    (apify/PuppeteerCrawler. (js-obj "requestList" r
-                                     "handlePageFunction" h))))
+(defn create-crawler [{:keys [request-queue request-list handler]}]
+  (let [maxR    (or maxRequests 100)
+        maxC    (or maxConcurrency 1)
+        _ (println handler)]
+    (apify/PuppeteerCrawler. (js-obj "requestList" request-list
+                                     "requestQueue" request-queue
+                                     "handlePageFunction" (promisify handler)))))
 
 (defn create-request-list [& urls]
-  (println (to-array urls))
   (go
+    (println urls)
     (<p! (apify/openRequestList "my-list",  (to-array urls)))))
 
-(defn handle-page-fn [options]
+(defn handle-page-fn [options cb]
   (go
     (let [page    (.-page options)
           request (.-request options)
           html    (<p! (.content page))]
-      (println "The title is" html)
-      (println "The request uniqueKey: " (.-uniqueKey request)))))
+      (println "Stuff is happening" (<p! (.title page)))
+      (cb))))
 
 (defn default-crawler [url]
   (go
+    (println "Url: " url)
     (let [list (<! (create-request-list url))]
-      (create-crawler handle-page-fn list))))
+      (create-crawler {:handler handle-page-fn
+                       :request-list list}))))
 
 (defn run-crawler [crawler]
   (go
     (let [cr (<! crawler)]
-      (.run cr))))
+      (println "Hello")
+      (println "The crawler is: " (<p! (.run cr))))))
